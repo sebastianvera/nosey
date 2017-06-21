5c5b93aa922b45af84c3f98e797896c2250e5f2e
Sebastian Vera <u.verainf@gmail.com>
2017-06-20 21:00:04 -0400

    Delete parsers

--- src/parsers/index.js
+++
@@ -1,9 +0,0 @@
-import JavascriptParser from './js';
-import VimParser from './vim';
-
-export const parsersByExtension = {
-  js: new JavascriptParser(),
-  vim: new VimParser(),
-};
-
-export default parsersByExtension;
